3b482d835e680d88ef41708c30e88a5ba4b749b0
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-10-21 16:14:36 -0500

    Intro to Java Exercise complete

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+//System.out.println is used to output information to the console with a newline.
+
+// public class HelloWorld {
+//    public static void main(String[] args){
+//        System.out.println("Hello, World!");
+//
+//    }
+//}
+
+//System.out.print can be used to output information without a newline. Refactor your code to use two System.out.print statements without changing the output.
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello, ");
+        System.out.print("World!");
+
+    }
+}
