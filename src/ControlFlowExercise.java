06281eb2e3e90c4e14dd717f84d676302f414a7a
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-10-25 12:01:08 -0500

    feat: Control Statements and Loops Exercise 1b Complete

--- src/ControlFlowExercise.java
+++ src/ControlFlowExercise.java
@@ -14,17 +14,34 @@ public class ControlFlowExercise {
         // 5 6 7 8 9 10 11 12 13 14 15
 
         //Solution 1a:
-        int i = 5;
+        // int i = 5;
 
-        while (i <= 15) {
-            System.out.println("i is " + i);
-            i++;
-        }
+        // while (i <= 15) {
+            // System.out.println("i is " + i);
+            // i++;
+        // }
 
         //b. Do While
 
         // Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+        //Solution 1b:
+        // int i = 0;
+
+        // do {
+            // System.out.println(i);
+            // i += 2;
+        // } while (i <= 100);
+
         // Alter your loop to count backwards by 5's from 100 to -10.
+
+        //Solution:
+        // int num = 100;
+
+        // do {
+            // System.out.println(num);
+            // num = num - 5;
+        // } while (num >= -10);
+
         // Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
 
         // 2
@@ -33,14 +50,28 @@ public class ControlFlowExercise {
         // 256
         // 65536
 
-        //Solution 1b:
+        //Solution:
 
+        // long a = 2;
+
+        // do {
+            // System.out.println(a);
+            // a *= a;
+        // } while (a < 1000000);
 
         //c. For
 
         // Refactor the previous two exercises to use a for loop instead.
 
         //Solution 1c:
+
+        // for(int i = 5; i <= 15; i++) {
+            // System.out.println(i);
+        // }
+
+        // for(int i = 0; i <= 100; i+=2) {
+            // System.out.println(i);
+        // }
 
         //2. Fizzbuzz
 
